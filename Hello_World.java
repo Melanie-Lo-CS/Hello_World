9eefb3aab24c3fb046f7788e64ce08f1cb1854c8
Melanie-Lo-CS <melaniekwanyeelo@gmail.com>
2018-12-18 11:57:24 -0700

    acsacv

--- Hello_World.java
+++ Hello_World.java
@@ -1,8 +1,46 @@
+/**<h1> HELLO WORLD </h1>
+*<b>LeARnInG</b><br>
+*<p></p>
+*<b>How to start the project instructions:</b><br>
+*<p></p>
+*<b>User Instructions:<b/><br>
+*<p></P>
+*<br>
+*@author Melanie
+*@version 1
+*@since 2018-12-11
+*/
+import java.io.*;
+
 public class Hello_World
 {
-   public static void main (String[] args)
-   {
-     System.out.println("hello world");
-   }
+  /**<b>Class Description: Necessary Class</b><br>
+  *<p></p>
+  *@param
+  *@return
+  *@throws
+  */
+
+  /**<b>Method Description: Main, Driver</b><br>
+  *<p></p>
+  *@param args unused
+  *@return
+  *@throws
+  */
+
+      public int Hello_World(int numA, int numB)
+      {
+        return numA + numB;
+      }
+      public static void main(String args[])
+      throws IOException
+     {
+       AddNum obj = new Hello_World();
+       int sum= obj.Hello_World(10,20);
+
+       System.out.println("\nSum of 10 and 20 is " + sum);
+     }
+
+    }
 
 }
